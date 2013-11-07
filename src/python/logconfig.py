d1e5a8ff364b96c1d8a2d7b540be40e149ea1c7d
James <jagleeso@gmail.com>
2013-11-07 00:34:00 -0500

    add simple clementine cmdproc

--- src/python/logconfig.py
+++ src/python/logconfig.py
@@ -1,3 +1,14 @@
 import logging.config
 
-logging.basicConfig(level=logging.INFO)
+# logging.basicConfig(level=logging.INFO)
+
+import logging,sys
+formatter = logging.Formatter('%(filename)s:%(lineno)s :: %(message)s')
+stdout_handler = logging.StreamHandler(sys.stdout)
+stdout_handler.setFormatter(formatter)
+logger=logging.getLogger('')
+logger.addHandler(stdout_handler)
+logger.setLevel(logging.INFO)
+# logging.debug('A debug message')
+# logging.info('Some information')
+# logging.warning('A shot across the bows')
